556af2f4e294fc4b4febcbfe0484ca3a7b24bc83
Raurik <262612180+Raurik@users.noreply.github.com>
2026-02-26 15:14:03 +0000

    chore: consolidate 22 SQL migration/fix files into MIGRATIONS_ALL.sql

--- MIGRATION_PRODUCTS_POWER_TYPE.sql
+++
@@ -1,8 +0,0 @@
--- Migration: Add power_type column to products table
--- Run against Railway PostgreSQL
--- Date: 2025-01-26
-
-ALTER TABLE hearmed_reference.products
-ADD COLUMN IF NOT EXISTS power_type VARCHAR(30) DEFAULT NULL;
-
-COMMENT ON COLUMN hearmed_reference.products.power_type IS 'Hearing aid power type: Rechargeable, 312 Battery, 13 Battery, 10 Battery, 675 Battery';
