182af05a4b865e73e4a399b98a4d1feaf553b7b8
Raurik <262612180+Raurik@users.noreply.github.com>
2026-02-26 11:30:31 +0000

    Bell widget: grey default, gold glow + shake on hover via currentColor

--- modules/mod-notifications.php
+++ modules/mod-notifications.php
@@ -390,7 +390,7 @@ function hm_notifications_bell_widget() {
     <div id="hm-bell-widget" style="display:none;">
         <a href="/notifications/" id="hm-bell-link" title="Notifications">
             <svg id="hm-bell-svg" xmlns="http://www.w3.org/2000/svg" width="22" height="22"
-                 viewBox="0 0 24 24" fill="none" stroke="#F5C218" stroke-width="2"
+                 viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                  stroke-linecap="round" stroke-linejoin="round">
                 <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/>
                 <path d="M13.73 21a2 2 0 0 1-3.46 0"/>
